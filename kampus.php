13f589690fd2490d33363617c9666ac9201b5806
ranaoktaviani <160466637+ranaoktaviani@users.noreply.github.com>
2024-03-06 14:40:56 +0700

    Create kampus.php
    file data kampus

---
+++ kampus.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <title>Kampus</title>
+  </head>
+  <body>
+    <?php
+      echo "Amik Mahaputra Riau>";
+      echo " Alamat : Jl.Soebrantas";
+      echo " Nama : Rana Oktaviani";
+?>
+  </body>
+</html>
