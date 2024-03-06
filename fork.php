afc3e843d4873669860bab607c67ed9a96e40ecd
AziZaitulMukaramah <159240082+AziZaitulMukaramah@users.noreply.github.com>
2024-03-06 14:44:09 +0700

    Update fork.php
    Update Azi Zaitul Mukaramah

--- fork.php
+++ fork.php
@@ -5,6 +5,8 @@
   <body>
     <?php
       echo "Fork adalah Membuat ‘copy/cuplikat’ dari repo orang lain (beserta historynya)";
+      echo "Jembatan antara repo original dan duplikatnya.";
+      echo "Azi Zaitul Mukaramah";
     <?
   </body>
 </html>
