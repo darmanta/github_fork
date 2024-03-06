c051e636ae683805b1ee08f5049d24db82428e09
firman22 <160466932+skuyui@users.noreply.github.com>
2024-03-06 14:41:18 +0700

    Create kampus.php
    Firmansyah

---
+++ kampus.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>Kampus</title>
+</head>
+<body>
+
+  <?php
+  echo "Amik Mahaputra Riau<br>";
+  echo "Alamat : JL. Soebrantas";
+  echo "Nama   : Firmansyah ";
+  ?>
+
+</body>
+</html>
