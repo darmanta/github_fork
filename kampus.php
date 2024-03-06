54df427b9f8bc38545e49cfe5fc61cce4d61afaa
ciitra <159239257+ciitra@users.noreply.github.com>
2024-03-06 14:40:55 +0700

    Create kampus.php
    File data kampus citra

---
+++ kampus.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <tittle>Kampus</tittle>
+     </head>
+    <body>
+      <?php
+        echo "Amik Mahaputra Riau";
+        echo "Alamat: JlL.Soebrantas";
+        echo "Nama: Citra Ameilia";
+      ?>
+    </body>
+</html>
