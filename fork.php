7fb44de697c768ad1f725d6c03d16d75e578b251
wahyumuhari22 <159239060+wahyumuhari22@users.noreply.github.com>
2024-03-06 14:44:05 +0700

    Update fork.php
    update wahyu muhari muhammad

--- fork.php
+++ fork.php
@@ -4,7 +4,8 @@
   </head>
   <body>
     <?php
-      echo "Fork adalah Membuat ‘copy/cuplikat’ dari repo orang lain (beserta historynya)";
+      echo "Fork adalah Membuat ‘copy/duplikat’ dari repo orang lain (beserta historynya)<br>";
+      echo "Wahyu Muhari Muhamad";
     <?
   </body>
 </html>
