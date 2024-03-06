e4c946e3a2aff81bcf63a068a0a767b31a01db79
prasetiyabimanandaputra <159242187+prasetiyabimanandaputra@users.noreply.github.com>
2024-03-06 15:21:00 +0700

    Create edit.php
    edit data kampus

---
+++ edit.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <tille>kampus</tille>
+  </head>
+  <body></body>
+  <?php
+echo "Fremwok adalah membuat 'copy/cuplikan dari respon orang lain(beserta historinya)<br>";
+echo "jembatan antara  respon  original dan cuplikan";
+?>
+</html>
+</body>
